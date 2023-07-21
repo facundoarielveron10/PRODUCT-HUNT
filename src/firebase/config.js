3939cadd3904b2509e3ed0046ce1d27c928e8bd1
Facundo Veron <facu_ve_2016@hotmail.com>
2023-07-20 21:25:02 -0300

    Agregando ENV

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 // ---- CONFIGURACION FIREBASE ---- //
 const firebaseConfig = {
-    apiKey: 'AIzaSyC-yVGrTdLmDqqJzMrUQnfLqUo6pFaCnC4',
-    authDomain: 'product-hunt-d1dd3.firebaseapp.com',
-    projectId: 'product-hunt-d1dd3',
-    storageBucket: 'product-hunt-d1dd3.appspot.com',
-    messagingSenderId: '881083614043',
-    appId: '1:881083614043:web:cfae4ca068dcb20747107e',
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
 };
 // -------------------------------- //
 
